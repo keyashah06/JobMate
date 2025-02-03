770c21a926a7a375220f428fd3b08e14aeccb4af
Hibah Arshad <146794254+HibahArshad@users.noreply.github.com>
2025-02-02 22:51:39 -0500

    Login/Sign Up UI
    First version of UI

---
+++ jobmate-app/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import { TbUser, TbMail, TbLock } from "react-icons/tb";
+import './LoginSignup.css'
+//import "./styles.css"; 
+
+const LoginSignup = () => {
+  const [action,setAction] = useState("Login"); 
+  
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+        <TbUser size={20} />  
+          <input type="text" placeholder="Username" />
+        </div>}
+        
+      
+        <div className="input">
+          <TbMail size={20} />  
+          <input type="email" placeholder="Email Id" />
+        </div>
+        <div className="input">
+          <TbLock size={20} />  
+          <input type="password" placeholder="Password" />
+      </div>
+     </div> 
+     {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
+     <div className="submit-container">
+      <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+      <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+     </div>
+    </div>
+  );
+};
+
+export default LoginSignup
